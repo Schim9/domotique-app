cb711b6962442b6211913aa51258a10ce6f171c3
Eric <eric@kaminski.lu>
2023-09-28 18:07:40 +0200

    - Add keen-slider - refacto + clean

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild} from '@angular/core';
+import KeenSlider, {KeenSliderInstance} from "keen-slider"
 import {DomoticzItem} from "../../models/domoticz-item.model";
 
 @Component({
@@ -6,9 +7,25 @@ import {DomoticzItem} from "../../models/domoticz-item.model";
   templateUrl: './carrousel.component.html',
   styleUrls: ['./carrousel.component.scss']
 })
-export class CarrouselComponent {
+export class CarrouselComponent implements AfterViewInit, OnDestroy{
 
   @Input() element: DomoticzItem[]
 
-  protected readonly interval = 0;
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>
+
+  slider: KeenSliderInstance | null = null
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef?.nativeElement, {
+      initial: 0,
+      slides: {
+        perView: 3,
+        spacing: 24,
+      },
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
 }
