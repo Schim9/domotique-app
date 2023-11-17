91d8393946757d5ebb5ba948fd6bc7881e38500a
Eric <eric@kaminski.lu>
2023-11-17 16:17:28 +0100

    - Fix

--- src/app/components/switch-button/switch-button.component.ts
+++ src/app/components/switch-button/switch-button.component.ts
@@ -38,34 +38,23 @@ export class SwitchButtonComponent implements OnChanges {
   }
 
   defineIcon = (): string => {
-    let valueToCheck = `${this.elementAsTmp.image}_${this.elementAsTmp.status}`
-    switch (valueToCheck) {
-      case "Light_On":
-        return "./assets/switch/Light48_On.png"
-      case "Light_Off":
-        return "./assets/switch/Light48_Off.png"
-      case "ChristmasTree_On":
-        return "./assets/switch/ChristmasTree48_On.png"
-      case "ChristmasTree_Off":
-        return "./assets/switch/ChristmasTree48_Off.png"
-      case "Fireplace_On":
-        return "./assets/switch/Fireplace48_On.png"
-      case "Fireplace_Off":
-        return "./assets/switch/Fireplace48_Off.png"
-      case "Fan_On":
-        return "./assets/switch/Fan48_On.png"
-      case "Fan_Off":
-        return "./assets/switch/Fan48_Off.png"
-      case "Push_On":
-        return "./assets/switch/Push48_On.png"
-      case "Push_Off":
-        return "./assets/switch/Push48_Off.png"
+    switch (this.elementAsTmp.image) {
+      case "Light":
+      case "ChristmasTree":
+      case "Fireplace":
+      case "Fan":
+      case "Push":
+        return this.defineKnownIcon()
       default:
-        if (this.elementAsTmp.status == "On") {
-          return "./assets/switch/Generic48_On.png"
-        } else {
-          return "./assets/switch/Generic48_Off.png"
-        }
+        return `./assets/switch/Generic48_${this.elementAsTmp.status}.png`
+    }
+  }
+
+  private defineKnownIcon = () : string => {
+    if (this.elementAsTmp.status == "Off") {
+      return `./assets/switch/${this.elementAsTmp.image}48_Off.png`
+    } else {
+      return `./assets/switch/${this.elementAsTmp.image}48_On.png`
     }
   }
 }
