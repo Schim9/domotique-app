393e817a680a5bd69b1b5b3538673beb811e4bc0
Eric <eric@kaminski.lu>
2024-05-21 23:12:25 +0200

    feat(): fix refresh issue

--- src/app/components/side-nav-bar/sidenav.component.ts
+++ src/app/components/side-nav-bar/sidenav.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, Output, EventEmitter, inject} from "@angular/core";
 import {ToolboxService} from "../../services/toolbox.service";
-import {DomoticzApiService} from "../../services/domoticz-api.service";
 
 @Component({
   selector: "side-nav",
@@ -12,15 +11,15 @@ export class SidenavComponent {
   @Output() toggleMenu = new EventEmitter();
 
   private toolBoxService: ToolboxService = inject(ToolboxService)
-  private domoticzService: DomoticzApiService = inject(DomoticzApiService)
 
   public routeLinks = this.toolBoxService.routeLinks
 
   handleClick() {
+    this.toolBoxService.getRefreshTrigger().emit()
     // In case the side nav bar is open
     // We close it before routing.
     // Otherwise, the keen slider's rendering will be awful
-    this.domoticzService.fetchAllElements().subscribe()
+    // this.domoticzService.fetchAllElements().subscribe()
     if (this.isExpanded)
       this.toggleMenu.emit()
   }
